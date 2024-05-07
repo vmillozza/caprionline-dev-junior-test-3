5b6127e24db1a15b1bf14c9a3c5e47b7fbf71766
vmillozza <142731369+vmillozza@users.noreply.github.com>
2024-05-07 07:57:04 +0200

    Add files via upload
    Il compito consiste nell'implementare i filtri e l'ordinamento per una pagina web che mostra un elenco di film di ultima uscita, utilizzando come tecnologie principali Symfony, React e Tailwind.

---
+++ frontend/src/MoviesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class MoviesController extends AbstractController
+{
+    public function __construct(
+        private MovieRepository $movieRepository,
+        private SerializerInterface $serializer
+    ) {}
+    
+    #[Route('/movies', methods: ['GET'])]
+    public function list(Request $request): JsonResponse
+    {
+        $year = $request->query->get('year');
+        $sort = $request->query->get('sort');
+
+        $criteria = [];
+        if ($year) {
+            $criteria['year'] = $year;
+        }
+
+        $orderBy = [];
+        if ($sort) {
+            $sortParts = explode('_', $sort);
+            if (count($sortParts) == 2) {
+                $orderBy[$sortParts[0]] = $sortParts[1] === 'desc' ? 'DESC' : 'ASC';
+            }
+        }
+
+        $movies = $this->movieRepository->findBy($criteria, $orderBy);
+        $data = $this->serializer->serialize($movies, "json", ["groups" => "default"]);
+
+        return new JsonResponse($data, json: true);
+    }
+}
